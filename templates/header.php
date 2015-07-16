6b588e697a232e0b1de97c0957228d7b429c7fb6
elvis <elvina@safetycat.org>
2015-07-16 13:47:51 +0100

    added bootstrap nav walker and basic config settings

--- templates/header.php
+++ templates/header.php
@@ -2,9 +2,9 @@
   <div class="container">
     <a class="brand" href="<?= esc_url(home_url('/')); ?>"><?php bloginfo('name'); ?></a>
     <nav role="navigation">
-      <?php
+       <?php
       if (has_nav_menu('primary_navigation')) :
-        wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav']);
+        wp_nav_menu(['theme_location' => 'primary_navigation', 'walker' => new wp_bootstrap_navwalker(), 'menu_class' => 'nav navbar-nav']);
       endif;
       ?>
     </nav>
